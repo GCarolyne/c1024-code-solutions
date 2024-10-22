a13c3261652b9990ce42d2042c9de40200f12c82
Carolyne Golias <goliascarolyne@gmail.com>
2024-10-22 22:30:15 +0000

    completed typescript-arrays

---
+++ typescript-arrays/main.js
@@ -0,0 +1,18 @@
+'use strict';
+const colors = ['red', 'white', 'blue'];
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]:', colors[1]);
+console.log('value of colors[2]:', colors[2]);
+console.log(`America is ${'red'}, ${'white'}, and ${'blue'}.`);
+colors[2] = 'green';
+console.log(`Mexico is ${colors[0]}, ${colors[1]}, and ${colors[2]}`);
+console.log('value of colors', colors);
+console.log('the type of colors are', typeof colors);
+const students = ['Bobby', 'Tim', 'Tommy'];
+const numberOfStudents = students.length;
+console.log(`There are ${numberOfStudents} students in the class.`);
+const lastIndex = [numberOfStudents - 1];
+const lastStudent = students[lastIndex];
+console.log(`The last student in the array is ${lastStudent}`);
+console.log('value of students', students);
+console.log('the type of', typeof students);
