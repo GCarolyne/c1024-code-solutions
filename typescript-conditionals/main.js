69797c59929ba2ab2945c247688af05f1dc07d43
Carolyne Golias <goliascarolyne@gmail.com>
2024-10-24 22:06:12 +0000

    completed typescript-conditionals

---
+++ typescript-conditionals/main.js
@@ -0,0 +1,94 @@
+'use strict';
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isEven(number) {
+  if (number % 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isOldEnoughToDrink(person) {
+  if (person.age > 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isOldEnoughToDrive(person) {
+  if (person.age > 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isOldEnoughToDrinkAndDrive(person) {
+  if (person) {
+    return false;
+  } else {
+    return false;
+  }
+}
+function categorizeAcidity(pH) {
+  if (pH === 7) {
+    return 'neutral';
+  } else if (pH <= 7) {
+    return 'acid';
+  } else if (pH > 7 && pH <= 14) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+}
+function introduceWarnerBro(name) {
+  switch (name) {
+    case 'yakko':
+      return 'we are the warner brothers!';
+    case 'wakko':
+      return 'We are the warner brothers';
+    case 'dot':
+      return 'Im cute';
+    default:
+      return 'Goodnight everybody!';
+  }
+}
+function recommendMovie(genre) {
+  switch (genre) {
+    case 'action':
+      return 'Die Hard';
+    case 'comedy':
+      return 'Step Brothers';
+    case 'horror':
+      return 'The Shining';
+    case 'drama':
+      return 'Forrest Gump';
+    case 'musical':
+      return 'The Sound Of Music';
+    case 'sci -fi':
+      return 'The Matrix';
+    default:
+      return 'Genre not recognized. Choose between action,comedy,horror,drama,musical, or sci-fi';
+  }
+}
---
+++ typescript-conditionals/main.ts
@@ -0,0 +1,108 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+function isUnderFive(number: number): boolean {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number: number): boolean {
+  if (number % 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string: string): boolean {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+function isOldEnoughToDrink(person: Person): boolean {
+  if (person.age > 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person: Person): boolean {
+  if (person.age > 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person: Person): boolean {
+  if (person) {
+    return false;
+  } else {
+    return false;
+  }
+}
+
+function categorizeAcidity(pH: number): string {
+  if (pH === 7) {
+    return 'neutral';
+  } else if (pH <= 7) {
+    return 'acid';
+  } else if (pH > 7 && pH <= 14) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+}
+
+function introduceWarnerBro(name: string): string {
+  switch (name) {
+    case 'yakko':
+      return 'we are the warner brothers!';
+
+    case 'wakko':
+      return 'We are the warner brothers';
+
+    case 'dot':
+      return 'Im cute';
+
+    default:
+      return 'Goodnight everybody!';
+  }
+}
+
+function recommendMovie(genre: string): string {
+  switch (genre) {
+    case 'action':
+      return 'Die Hard';
+    case 'comedy':
+      return 'Step Brothers';
+    case 'horror':
+      return 'The Shining';
+    case 'drama':
+      return 'Forrest Gump';
+    case 'musical':
+      return 'The Sound Of Music';
+    case 'sci -fi':
+      return 'The Matrix';
+    default:
+      return 'Genre not recognized. Choose between action,comedy,horror,drama,musical, or sci-fi';
+  }
+}
