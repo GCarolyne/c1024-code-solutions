d671fff3eab11c69712bd3f8359e4f6796e70955
Carolyne Golias <goliascarolyne@gmail.com>
2024-10-28 00:34:46 +0000

    completed array-manipulation-1

--- array-manipulation-1/src/head.ts
+++ array-manipulation-1/src/head.ts
@@ -1,1 +1,8 @@
 /* exported head */
+function head(array: any): any[] {
+  const newArray: any = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[0]);
+  }
+  return array;
+}
