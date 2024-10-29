a76eceb113790acbbaa39318d9de567ac9db6337
Carolyne Golias <goliascarolyne@gmail.com>
2024-10-29 01:24:25 +0000

    completed typescript-loops-practice

--- typescript-loops-practice/odd-or-even.ts
+++ typescript-loops-practice/odd-or-even.ts
@@ -1,9 +1,1 @@
 /* exported oddOrEven */
-// function oddOrEven(numbers: number[]): string[] {
-// const newArray: string = [];
-// for (let i = 0; i < numbers.length; i++) {
-// if (numbers % 2 === 0) {
-// newArray.push;
-// }
-// }
-// }
