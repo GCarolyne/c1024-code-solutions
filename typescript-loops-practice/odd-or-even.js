a9e164e6c7e11aa4e3da638f57814a8e130e5025
Carolyne Golias <goliascarolyne@gmail.com>
2024-10-30 00:16:54 +0000

    completed typescript-loops-practice

--- typescript-loops-practice/odd-or-even.js
+++ typescript-loops-practice/odd-or-even.js
@@ -1,10 +1,20 @@
 'use strict';
 /* exported oddOrEven */
-// function oddOrEven(numbers: number[]): string[] {
-// const newArray: string = [];
-// for (let i = 0; i < numbers.length; i++) {
-// if (numbers % 2 === 0) {
-// newArray.push;
-// }
-// }
-// }
+function oddOrEven(numbers) {
+  const newArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      newArray.push('even');
+    } else {
+      newArray.push('odd');
+    }
+  }
+  return newArray;
+}
+// write out and think through the steps to know what steps you need to take.
+// create a new array and assign to a variable.
+// loop over the input array starting at beginning and running for the full length
+// check if the current number is even
+// if so push "even" to my new array
+// else push "odd" to my new array.
+// Once the loop finishes return newArray.
