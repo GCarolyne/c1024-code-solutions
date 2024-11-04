08fa59f2c0d46e778e902948702d5506578fffdc
Carolyne Golias <goliascarolyne@gmail.com>
2024-11-04 17:50:26 +0000

    completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const $element = document.querySelector('h1');
+console.log('$element');
+console.dir($element);
+const $explain = document.querySelector('#explanation');
+console.log('$explain');
+console.dir($explain);
+const $hint = document.querySelector('.hint');
+console.log('.hint');
+console.dir($hint);
+const $top = document.querySelectorAll('p');
+console.log('$top');
+console.dir($top);
+const $node1 = document.querySelectorAll('.example-link');
+console.log('$node1');
+console.dir($node1);
