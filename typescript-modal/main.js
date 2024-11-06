f1c3ae24b26808c23b6479ec1e2afa48ac8e6034
Carolyne Golias <goliascarolyne@gmail.com>
2024-11-06 18:23:57 +0000

    completed typescript-modal

---
+++ typescript-modal/main.js
@@ -0,0 +1,21 @@
+'use strict';
+const $openModal = document.querySelector('.open-modal');
+const $disModal = document.querySelector('.dismiss-modal');
+const $dialog = document.querySelector('dialog');
+if (!$openModal) throw new Error('$openModal does not exist');
+if (!$disModal) throw new Error('$disModal');
+if (!$dialog) throw new Error('$dialog does not exist');
+function handleClick(event) {
+  console.log('event', event);
+  console.log('event.target', event.target);
+  if (!$dialog) throw new Error('!$dialog does not exist');
+  $dialog.showModal();
+}
+$openModal.addEventListener('click', handleClick);
+function dismissClick(event) {
+  console.log('event', event);
+  console.log('event.target', event.target);
+  if (!$dialog) throw new Error('!$dialog does not exist');
+  $dialog.close();
+}
+$disModal.addEventListener('click', dismissClick);
