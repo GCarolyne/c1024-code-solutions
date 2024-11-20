5dde78280b1c1dad98d8dc40c98e7e8d1bc30508
Carolyne Golias <goliascarolyne@gmail.com>
2024-11-20 00:54:22 +0000

    array-methods

---
+++ array-methods/filter.ts
@@ -0,0 +1,15 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+console.log(numbers);
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna',
+];
+console.log(names);
+// n%2===0
