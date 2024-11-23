d4f5724f79e6a099f46ef27138ff50bd3946f8d4
Carolyne Golias <goliascarolyne@gmail.com>
2024-11-23 01:27:50 +0000

    testing api

--- fetch/main.js
+++ fetch/main.js
@@ -1,18 +1,56 @@
 'use strict';
-async function fetchData() {
-  console.log('running');
+// interface User {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+// }
+// async function fetchData(): Promise<void> {
+//   console.log('running');
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+//       method: 'GET',
+//     });
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     const users = (await response.json()) as User[];
+//     console.log(users);
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// }
+// console.log('users');
+// fetchData();
+async function fetchArtObjects() {
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-      method: 'GET',
-    });
+    const response = await fetch(
+      'https://api.artic.edu/api/v1/artworks?limit=2'
+    );
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
-    const users = await response.json();
-    console.log(users);
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
-    console.error('Error:', error);
+    console.error('An error occurred:', error);
   }
 }
-console.log('users');
-fetchData();
+fetchArtObjects();
+async function fetchPoetObjects() {
+  try {
+    const response = await fetch('https://poetrydb.org/title/Sonnet');
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+fetchPoetObjects();
+// // const imageUrl = data.config.iiif_url;
+// // const imageId = data.config.image_id;
+// const default = data.config.
+// // console.log(imageUrl,imageId)
--- fetch/main.ts
+++ fetch/main.ts
@@ -1,25 +1,61 @@
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
+// interface User {
+//   id: number;
+//   name: string;
+//   username: string;
+//   email: string;
+// }
 
-async function fetchData(): Promise<void> {
-  console.log('running');
+// async function fetchData(): Promise<void> {
+//   console.log('running');
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+//       method: 'GET',
+//     });
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     const users = (await response.json()) as User[];
+//     console.log(users);
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// }
+
+// console.log('users');
+// fetchData();
+
+async function fetchArtObjects(): Promise<void> {
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-      method: 'GET',
-    });
+    const response = await fetch(
+      'https://api.artic.edu/api/v1/artworks?limit=2'
+    );
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
-    const users = (await response.json()) as User[];
-    console.log(users);
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
-    console.error('Error:', error);
+    console.error('An error occurred:', error);
+  }
+}
+fetchArtObjects();
+
+async function fetchPoetObjects(): Promise<void> {
+  try {
+    const response = await fetch('https://poetrydb.org/title/Sonnet');
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('An error occurred:', error);
   }
 }
 
-console.log('users');
-fetchData();
+fetchPoetObjects();
+
+// // const imageUrl = data.config.iiif_url;
+// // const imageId = data.config.image_id;
+// const default = data.config.
+// // console.log(imageUrl,imageId)
